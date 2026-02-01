62827f307d12b0017c1f50d6a1c6a51518d7b0aa
Aarav Maloo <aaravmaloo06@gmail.com>
2026-02-01 22:20:23 +0530

    Add src/cmd/self.go: Self-management commands

---
+++ src/cmd/self.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var selfCmd = &cobra.Command{
+	Use:   "self",
+	Short: "Manage xe itself",
+}
+
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Update xe to the latest version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Checking for updates...")
+		fmt.Println("xe is already up to date (v0.1.0)")
+	},
+}
+
+func init() {
+	selfCmd.AddCommand(updateCmd)
+	rootCmd.AddCommand(selfCmd)
+}
